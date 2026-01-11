059f00b686f29bf24e74c6042935045a150400d0
mspark <mspark@maprex.co.kr>
2026-01-11 14:25:53 +0900

    chore: Storybook 설정과 의존성 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,20 @@
 import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
 import react from "@vitejs/plugin-react";
+import { storybookTest } from "@storybook/addon-vitest/vitest-plugin";
 import tailwind from "tailwindcss";
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [react(), mode === "development" && screenGraphPlugin()],
+  plugins: [react(), mode === "development" && screenGraphPlugin(), storybookTest()],
   publicDir: "./static",
   base: "./",
   css: {
     postcss: {
       plugins: [tailwind()],
     },
+  },
+  test: {
+    setupFiles: ["./.storybook/vitest.setup.ts"],
   },
 }));
